046a85a89a16e8da1315ef4fb9730070d278eac3
baba_s <shotababa1206@gmail.com>
2020-07-10 20:06:47 +0900

    first commit

---
+++ Editor/AssetPathMultipleCopyer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal static class AssetPathMultipleCopyer
+	{
+		private const string ITEM_NAME = @"Assets/Copy Path (Multiple)";
+
+		[MenuItem( ITEM_NAME )]
+		private static void FromAssets()
+		{
+			var paths = Selection.objects
+					.Select( x => AssetDatabase.GetAssetPath( x ) )
+					.OrderBy( x => x )
+				;
+
+			var text = string.Join( Environment.NewLine, paths );
+
+			EditorGUIUtility.systemCopyBuffer = text;
+		}
+
+		[MenuItem( ITEM_NAME, true )]
+		private static bool Validate()
+		{
+			var objects = Selection.objects;
+			return objects != null && 0 < objects.Length;
+		}
+	}
+}
