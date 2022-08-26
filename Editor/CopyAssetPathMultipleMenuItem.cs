c30693d9ae31dcdc08962854ed81e6e23dee84da
baba-s <shotababa1206@gmail.com>
2022-08-26 19:32:10 +0900

    update

---
+++ Editor/CopyAssetPathMultipleMenuItem.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class CopyAssetPathMultipleMenuItem
+    {
+        private const string MENU_ITEM_NAME = @"Assets/Copy Path (Multiple) %&c";
+
+        [MenuItem( MENU_ITEM_NAME, true )]
+        private static bool CanCopy()
+        {
+            return Selection.assetGUIDs is { Length: > 0 };
+        }
+
+        [MenuItem( MENU_ITEM_NAME )]
+        private static void Copy()
+        {
+            var assetGUIDs = Selection.assetGUIDs;
+
+            if ( assetGUIDs == null || assetGUIDs.Length <= 0 ) return;
+
+            if ( assetGUIDs.Length == 1 )
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath( assetGUIDs[ 0 ] );
+                EditorGUIUtility.systemCopyBuffer = assetPath;
+                Debug.Log( $"Copied! `{assetPath}`" );
+            }
+            else
+            {
+                var assetPaths = assetGUIDs
+                        .Select( x => AssetDatabase.GUIDToAssetPath( x ) )
+                        .OrderBy( x => x )
+                    ;
+
+                var result = string.Join( "\n", assetPaths );
+                EditorGUIUtility.systemCopyBuffer = result;
+                Debug.Log( $"Copied!\n```\n{result}\n```" );
+            }
+        }
+    }
+}
